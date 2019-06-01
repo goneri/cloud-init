9f44416048be5f69cf9a706f760c0b8a901c3182
Gonéri Le Bouder <goneri@lebouder.net>
2019-05-31 23:14:13 -0400

    set_passwords: support for FreeBSD
    Add FreeBSD support to `cc_set_passwords`.
    FreeBSD does not come with chpasswd binary, a chpasswd port[1] exists
    but it's not the same tool than the one from Linux[2].
    
    Finally, NetBSD and OpenBSD don't have the chpasswd binary at all, so
    an abstraction layer will be needed.
    
    This patch moves the Linux specific in the Distro class, this allow
    anyone to override the default implementation. It also provides an
    implementation for FreeBSD.
    
    [1]: https://www.freshports.org/www/chpasswd/
    [2]: https://github.com/shadow-maint/shadow/blob/master/src/chpasswd.c

--- cloudinit/distros/freebsd.py
+++ cloudinit/distros/freebsd.py
@@ -233,7 +233,7 @@ class Distro(distros.Distro):
         if passwd_val is not None:
             self.set_passwd(name, passwd_val, hashed=True)
 
-    def set_passwd(self, user, passwd, hashed=False):
+    def set_passwd(self, user, passwd, hashed=False, force_change=False):
         if hashed:
             hash_opt = "-H"
         else:
@@ -244,6 +244,13 @@ class Distro(distros.Distro):
                       data=passwd, logstring="chpasswd for %s" % user)
         except Exception as e:
             util.logexc(LOG, "Failed to set password for %s", user)
+            raise e
+
+    def force_passwd_change(self, user):
+        try:
+            util.subp(['pw', 'usermod', user, '-p', '01-Jan-1970'])
+        except Exception as e:
+            util.logexc(LOG, "Failed to set pw expiration for %s", user)
             raise e
 
     def lock_passwd(self, name):
